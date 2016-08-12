ad342827f12a9b5c938fe3a62933077766a58449
Che Mok <chemok78@gmail.com>
2016-08-12 04:20:12 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Welcome to Votely: a Voting App build with the MEAN stack + Chart JS
+
+## Functions:
+
+1) As an authenticated user, I can keep my polls and come back later to access them
+
+2) As an authenticated user, I can share my polls with friends
+
+3) As an authenticated user, I can see the aggregate results of my polls
+
+4) As an authenticated user, I can delete polls that I decide I don't want anymore
+
+5) As an authenticated user, I can create a poll with any number of possible items
+
+6) As an unauthenticated or authenticated user I can see and vote on everyone's polls
+
+7) As an unauthenticated or authenticated user, I can see the results of polls in chart form (using ChartJS)
+
+8) A an authenticated user, if I don't like the options on a poll, I can create a new option
---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+//load express module
+var path = require('path');
+//load path module to make path manipulation easier
+require('dotenv').config({silent: true});
+//load dotenv module to work with .env file
+var bodyParser = require('body-parser');
+//load body parser to parse incoming request bodies, available under req.body
+var mongodb = require('mongodb');
+//load native mongoDB driver
+var ObjectID = mongodb.ObjectID
+//load objectID method so we can do var objectId = new ObjectID
+
+var POLLS_COLLECTION = "polls";
+//set the variable POLLS_COLLECTION to the string "polls"
+
+var app = express();
+//intialize express and save the instance as variable app
+app.use(express.static(__dirname + "/public"));
+//use static middleware for public folders in the app instance
+app.use(bodyParser.json());
+//parse the request body as json in the app instance
+
+var db;
+//create a database variable outside of the database connection callback to reuse the connection
+
+mongodb.MongoClient.connect(process.env.DB_URL, function(err,database){
+//Connect to the database before starting the app server
+    
+    if(err){
+    //handle db connection error    
+        console.log(err);
+        process.exit(1);
+        //exit using global process with failure code 1 (0 is success code)
+        
+    }
+    
+    //Save database object as global variable
+    db = database;
+    console.log("Successfully connected to database");
+    
+    //Initialize app
+    var server = app.listen(process.env.PORT || 8080, function(){
+        
+        var port = server.address().port;
+        console.log("App is now running on port", port);
+        
+    });
+    
+
+});
